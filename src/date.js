89cdff1ca288a72ce0d81809a1a8108eea9282d2
David Lewis <davidlewisp@gmail.com>
2018-03-16 16:45:10 -0700

    write and fail spec to calculate age on mercury

--- src/date.js
+++ src/date.js
@@ -1,10 +1,3 @@
-export class Date {
-  constructor(year, month, day) {
-  this.year = year;
-  this.month = month - 1;
-  this.day = day;
-  }
-}
 
 export function yearsToSeconds(years) {
   let seconds = ((((years * 365) * 24) * 60) * 60);
@@ -12,10 +5,12 @@ export function yearsToSeconds(years) {
 }
 
 export function dateSpan(earlierDate, laterDate) {
-
   let milliseconds = laterDate - earlierDate;
-  console.log(earlierDate);
-  debugger;
   let seconds = milliseconds / 1000;
   return seconds;
+}
+
+export function mercuryAgeCalc(years) {
+  let mercuryAge = years  ;
+  return mercuryAge;
 }
--- src/date_spec.js
+++ src/date_spec.js
@@ -1,6 +1,6 @@
-import { Date } from './date.js';
 import { yearsToSeconds } from './date.js';
 import { dateSpan } from './date.js';
+import { mercuryAgeCalc } from './date.js';
 
 describe('yearsToSeconds', function() {
   it('Take a person’s age in years and convert it into seconds', function(){
@@ -12,11 +12,19 @@ describe('yearsToSeconds', function() {
 
 describe('dateSpan', function() {
   it('Take two dates and determine the difference, in seconds, between the two.', function(){
-    
+
     const testDate = new Date(1989, 2, 9);
     const earlierDate = new Date(1984, 10, 24);
-    debugger;
 
-    expect(dateSpan(earlierDate, testDate)).toEqual(1387584000);
+    expect(dateSpan(earlierDate, testDate)).toEqual(135302400);
   });
+});
+
+describe('mercuryAgeCalc', function() {
+  it('Return the age of a human in Mercury years.', function(){
+
+    let testAge = 44;
+
+    expect(mercuryAgeCalc(testAge)).toEqual(135302400);
+  }); 
 });
